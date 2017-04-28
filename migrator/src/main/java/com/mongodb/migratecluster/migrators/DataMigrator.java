6e6893a2f62c33cf7f95a9f1befe7f5b442682ed
Shyam Arjarapu <shyam.arjarapu@gmail.com>
2017-04-28 10:02:48 -0500

    reading and working works here. needs cleanup

--- migrator/src/main/java/com/mongodb/migratecluster/migrators/DataMigrator.java
+++ migrator/src/main/java/com/mongodb/migratecluster/migrators/DataMigrator.java
@@ -2,11 +2,19 @@ package com.mongodb.migratecluster.migrators;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.migratecluster.AppException;
 import com.mongodb.migratecluster.commandline.ApplicationOptions;
 import com.mongodb.migratecluster.commandline.Resource;
 import com.mongodb.migratecluster.commandline.ResourceFilter;
 import com.mongodb.migratecluster.observables.*;
+import com.mongodb.migratecluster.predicates.DatabaseFilterPredicate;
+import com.mongodb.migratecluster.predicates.CollectionFilterPredicate;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,13 +40,10 @@ public class DataMigrator {
     }
 
     private boolean isValidOptions() {
-        // source, target, oplog
-        if (
-                (this.options.getSourceCluster().equals("")) ||
-                (this.options.getTargetCluster().equals("")) ||
-                (this.options.getOplogStore().equals(""))
-            ) {
-            // invalid input
+        // option values for source, target, oplog must not be blank
+        if ((this.options.getSourceCluster().equals("")) ||
+            (this.options.getTargetCluster().equals("")) ||
+            (this.options.getOplogStore().equals(""))) {
             return false;
         }
         return true;
@@ -61,64 +66,78 @@ public class DataMigrator {
         //Map<String, List<Resource>> sourceResources = MongoDBIteratorHelper.getSourceResources(sourceClient);
         //Map<String, List<Resource>> filteredSourceResources = getFilteredResources(sourceResources);
 
-        //FilterIterable filterIterable = new FilterIterable(this.options.getBlackListFilter());
+        //DatabaseFilterPredicate filterIterable = new DatabaseFilterPredicate(this.options.getBlackListFilter());
 
-
-        // TODO: BUG; code not proceeding to below
         try {
             Date startDateTime = new Date();
             logger.info(" started processing at {}", startDateTime);
 
             readAndWriteResourceDocuments(sourceClient, targetClient);
-
+            //doATestRun(sourceClient);
 
             Date endDateTime = new Date();
+
             logger.info(" completed processing at {}", endDateTime);
             logger.info(" total time to process is {}", TimeUnit.SECONDS.convert(endDateTime.getTime() - startDateTime.getTime(), TimeUnit.MILLISECONDS));
+
         } catch (Exception e) {
             String message = "error in while processing server migration.";
             logger.error(message, e);
             throw new AppException(message, e);
         }
-        sourceClient.close();
         logger.info("Absolutely nothing should be here after this line");
     }
 
+    private void doATestRun(MongoClient client) {
+        Resource resource = new Resource("social", "user");
+        MongoCollection<Document> collection = client
+                .getDatabase(resource.getDatabase())
+                .getCollection(resource.getCollection());
+        DocumentIdReader reader = new DocumentIdReader(collection, resource);
+        Observable<List<ResourceDocument>> observable = reader
+                .buffer(1000)
+                .flatMap(new Function<List<Object>, ObservableSource<List<ResourceDocument>>>() {
+                    @Override
+                    public ObservableSource<List<ResourceDocument>> apply(List<Object> ids) throws Exception {
+                        return new DocumentsObservable(collection, resource, ids.toArray())
+                                .subscribeOn(Schedulers.io());
+                    }
+                });
+        observable
+            .blockingSubscribe(id -> {
+            logger.info("## found docId: {}", id.size());
+        });
+    }
+
     private void readAndWriteResourceDocuments(MongoClient sourceClient, MongoClient targetClient) {
-        // Note: Working; get the list of databases
+        // load the blacklist filters and create database and collection predicates
+        List<ResourceFilter> blacklistFilter = options.getBlackListFilter();
+        DatabaseFilterPredicate databasePredicate = new DatabaseFilterPredicate(blacklistFilter);
+        CollectionFilterPredicate collectionPredicate = new CollectionFilterPredicate(blacklistFilter);
+
         new DatabaseFlowable(sourceClient)
-                .filter(db -> {
-                    String database = "social";
-                    logger.info("db.name: [{}], string: [{}], comparision: [{}]", db.getString("name"), database, db.getString("name").equals(database));
-                    return db.getString("name").equalsIgnoreCase(database);
-                })
-                // Note: Working; for each database get the list of collections in it
+                .filter(databasePredicate)
                 .flatMap(db -> {
-                    logger.info(" => found database {}", db.getString("name"));
+                    logger.debug(" found database: {}", db.getString("name"));
                     return new CollectionFlowable(sourceClient, db.getString("name"));
-                    // Note: Working; CollectionFlowable::subscribeActual works as well
                 })
-                .filter(resource -> {
-                    String collection = "people";
-                    logger.info("collection.name: [{}], string: [{}], comparision: [{}]",
-                            resource.getCollection(), collection, resource.getCollection().equals(collection));
-                    return resource.getCollection().equalsIgnoreCase(collection);
-                })
+                .filter(collectionPredicate)
                 .map(resource -> {
-                    // Note: Nothing in here gets executed
                     logger.info(" ====> map -> found resource {}", resource.toString());
                     return new DocumentReader(sourceClient, resource);
-                    //return resource;
                 })
                 .map(reader -> {
                     logger.info(" ====> reader -> found resource {}", reader.getResource());
                     return new DocumentWriter(targetClient, reader);
                 })
                 .subscribe(writer -> {
-                    // Note: Nothing in here gets executed
+                    // block on the
                     logger.info(" ====> writer -> found resource {}", writer);
                     writer.blockingLast();
                 });
+                //TODO: BUG; looks like onNext is happening before the actual Ids are returned
+        sourceClient.close();
+        targetClient.close();
     }
 
     private boolean isEntireDatabaseBlackListed(String database) {
