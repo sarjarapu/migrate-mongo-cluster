4544fe063712fbecf1d4821793e86b14c6b2f938
Torsten Spindler <torsten.spindler@mongodb.com>
2019-10-09 15:50:50 +0200

    Fix operationTime, catch all exceptions to progress

--- migrator/src/main/java/com/mongodb/migratecluster/migrators/ChangestreamMigrator.java
+++ migrator/src/main/java/com/mongodb/migratecluster/migrators/ChangestreamMigrator.java
@@ -6,6 +6,7 @@ import org.bson.conversions.Bson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mongodb.MongoClient;
 import com.mongodb.MongoNamespace;
 import com.mongodb.ReadConcern;
 import com.mongodb.ReadPreference;
@@ -42,21 +43,22 @@ public class ChangestreamMigrator extends BaseMigrator {
 	private CollectionFilterPredicate collectionPredicate;
 	private final HashMap<String, Boolean> allowedNamespaces;
 	private BsonTimestamp operationTime;
-	
+
 	public ChangestreamMigrator(ApplicationOptions options) {
 		super(options);
-        List<ResourceFilter> blacklistFilter = options.getBlackListFilter();
-        databasePredicate = new DatabaseFilterPredicate(blacklistFilter);
-        collectionPredicate = new CollectionFilterPredicate(blacklistFilter);
-        allowedNamespaces = new HashMap<>();
-        ClientSession session = this.getSourceClient().startSession();
-        Document ping = new Document("ping", "1");
-        this.getSourceClient().getDatabase("admin").runCommand(ping);
-        operationTime = session.getOperationTime(); 
-        session.close();
-        if (operationTime == null) {
-        	logger.error("operationTime MUST NOT be null");
-        }
+		List<ResourceFilter> blacklistFilter = options.getBlackListFilter();
+		databasePredicate = new DatabaseFilterPredicate(blacklistFilter);
+		collectionPredicate = new CollectionFilterPredicate(blacklistFilter);
+		allowedNamespaces = new HashMap<>();
+		MongoClient client = this.getSourceClient();
+		ClientSession session = client.startSession();
+		Document ping = new Document("ping", "1");
+		client.getDatabase("admin").runCommand(session, ping);
+		operationTime = session.getOperationTime(); 
+		session.close();
+		if (operationTime == null) {
+			logger.error("operationTime MUST NOT be null");
+		}
 	}
 
 	@Override
@@ -65,7 +67,7 @@ public class ChangestreamMigrator extends BaseMigrator {
 		logger.info("SourceClient: {}", this.getSourceClient());
 		ChangeStreamIterable<Document> csi = this.getSourceClient().watch().fullDocument(FullDocument.UPDATE_LOOKUP).startAtOperationTime(operationTime);
 		ModificationHelper modificationHelper = new ModificationHelper(options);
-		
+
 		MongoCursor<ChangeStreamDocument<Document>> iterator = csi.iterator();
 		logger.info("Starting changestream");
 		while (iterator.hasNext()) {
@@ -82,68 +84,71 @@ public class ChangestreamMigrator extends BaseMigrator {
 					logger.info("Transferring to {} and {}", database, collection);
 					Document fullDoc = doc.getFullDocument();
 					MongoCollection<Document> coll = this.getTargetClient().getDatabase(database).getCollection(collection);
-					if (doc.getOperationType() == OperationType.INSERT) {
-						coll.insertOne(fullDoc);
-					} else
-					if (doc.getOperationType() == OperationType.UPDATE) {
-						Bson filter = eq("_id", fullDoc.get("_id"));
-						coll.replaceOne(filter, fullDoc);
-					} else
-					if (doc.getOperationType() == OperationType.DELETE) {
-						Bson filter = eq("_id", fullDoc.get("_id"));
-						coll.deleteOne(filter);
-					} else {
-						logger.warn("Unsupported OperationType: {}", doc.getOperationType());
+					try {
+						if (doc.getOperationType() == OperationType.INSERT) {
+							coll.insertOne(fullDoc);
+						} else
+							if (doc.getOperationType() == OperationType.UPDATE) {
+								Bson filter = eq("_id", fullDoc.get("_id"));
+								coll.replaceOne(filter, fullDoc);
+							} else
+								if (doc.getOperationType() == OperationType.DELETE) {
+									Bson filter = eq("_id", fullDoc.get("_id"));
+									coll.deleteOne(filter);
+								} else {
+									logger.warn("Unsupported OperationType: {}", doc.getOperationType());
+								}
+					} catch (Exception e) {
+						logger.error("Bravely ignoring exception {}", e.getMessage());
 					}
 				} else {
 					logger.error("process: Namespace without dot!");
 				}
-				
+
 			} else {
 				logger.trace("database/collection blacklisted {}", doc.getNamespace());
 			}
 
 		}	
 		logger.info("Exiting changestream");
-		
+
 	}
 
 	// Plain copy from OplogWriter
-    private boolean isNamespaceAllowed(String namespace) {
-        if (!allowedNamespaces.containsKey(namespace))
-        {
-            boolean allow = checkIfNamespaceIsAllowed(namespace);
-            allowedNamespaces.put(namespace, allow);
-        }
-        // return cached value
-        return allowedNamespaces.get(namespace);
-    }
+	private boolean isNamespaceAllowed(String namespace) {
+		if (!allowedNamespaces.containsKey(namespace))
+		{
+			boolean allow = checkIfNamespaceIsAllowed(namespace);
+			allowedNamespaces.put(namespace, allow);
+		}
+		// return cached value
+		return allowedNamespaces.get(namespace);
+	}
 
-    private boolean checkIfNamespaceIsAllowed(String namespace) {
-        String databaseName = namespace.split("\\.")[0];
-        try {
-            Document dbDocument = new Document("name", databaseName);
-            boolean isNotBlacklistedDB = databasePredicate.test(dbDocument);
-            if (isNotBlacklistedDB) {
-                // check for collection as well
-                String collectionName = namespace.substring(databaseName.length()+1);
-                Resource resource = new Resource(databaseName, collectionName);
-                return collectionPredicate.test(resource);
-            }
-            else {
-                return false;
-            }
-        } catch (Exception e) {
-            logger.error("error while testing the namespace is in black list or not");
-            return false;
-        }
-    }
-    
-    
+	private boolean checkIfNamespaceIsAllowed(String namespace) {
+		String databaseName = namespace.split("\\.")[0];
+		try {
+			Document dbDocument = new Document("name", databaseName);
+			boolean isNotBlacklistedDB = databasePredicate.test(dbDocument);
+			if (isNotBlacklistedDB) {
+				// check for collection as well
+				String collectionName = namespace.substring(databaseName.length()+1);
+				Resource resource = new Resource(databaseName, collectionName);
+				return collectionPredicate.test(resource);
+			}
+			else {
+				return false;
+			}
+		} catch (Exception e) {
+			logger.error("error while testing the namespace is in black list or not");
+			return false;
+		}
+	}
+
+
 	@Override
 	public void preprocess() {
 		// TS: not sure what to do here
 	}
 
 }
- 
